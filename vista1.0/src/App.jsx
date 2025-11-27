ed115a3816a652330e6d3e736368c662f88fcd79
iriscc7 <raffaella240906@gmail.com>
2025-11-26 20:50:24 -0500

    26-11-25
    Prueba 1

---
+++ vista1.0/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {Routes, Route, Link, NavLink} from 'react-router-dom'
+import Home from './views/Home.jsx'
+
+function App() {
+
+
+  return (
+    <>
+    {/* <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes> */}
+          <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ vista1.0/src/views/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = ()=>{
+    return(
+        <>
+        <h1>¿A dónde viajamos?</h1>
+        </>
+    )
+}
+
+export default Home 
